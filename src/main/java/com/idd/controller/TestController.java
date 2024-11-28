59890d5c01f79731f88668f9fff9009be4723add
Idd mohammad <mohammadidd345@gmail.com>
2024-11-28 11:24:51 +0530

    nahi hora yaar

--- src/main/java/com/idd/controller/TestController.java
+++ src/main/java/com/idd/controller/TestController.java
@@ -1,12 +1,13 @@
 package com.idd.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 	
-	@GetMapping("/test")
+	@RequestMapping("/test")
 	public String test() {
 		return "Jenkins Test is completed Successfully..";
 	}
