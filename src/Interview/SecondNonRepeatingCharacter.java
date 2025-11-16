76c69f9aecb4e754824f8ea2e9c790c9c37006ab
Idd mohammad <mohammadidd345@gmail.com>
2025-11-16 19:36:39 +0530

    Second Non-Repeating Character

--- src/Interview/SecondNonRepeatingCharacter.java
+++ src/Interview/SecondNonRepeatingCharacter.java
@@ -1,25 +1,31 @@
 package Interview;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class SecondNonRepeatingCharacter {
     public static void main(String[] args) {
-        String str="idd mohammad";
+        String str = "idd mohammad";
+
         Map<Character, Long> frequency = str.chars()
                 .mapToObj(c -> (char) c)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()));
+
+        System.out.println(frequency);
 
         Character c = frequency.entrySet()
                 .stream()
-                .filter(e -> e.getValue() < 2)
+                .filter(e -> e.getValue() == 1 && e.getKey() != ' ')
                 .map(Map.Entry::getKey)
                 .skip(1)
                 .findFirst()
-                .get();
+                .orElse(null);
 
         System.out.println(c);
-
     }
 }
